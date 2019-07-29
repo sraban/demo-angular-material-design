c532c1c924b3e6bcc0c7e3599cced4112a544d70
Sraban Kumar <kureshu.n@edkal.com>
2019-07-29 12:48:44 +0530

    attribute tested enable and disable and readonly of FormGroup

--- src/app/reactive-form-two/reactive-form-two.component.ts
+++ src/app/reactive-form-two/reactive-form-two.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component,OnInit , ViewChild, ElementRef} from '@angular/core';
 import { FormBuilder, FormGroup, Validators ,FormsModule,NgForm } from '@angular/forms';  
 
 @Component({
@@ -7,8 +7,12 @@ import { FormBuilder, FormGroup, Validators ,FormsModule,NgForm } from '@angular
   styleUrls: ['./reactive-form-two.component.css']
 })
 export class ReactiveFormTwoComponent implements OnInit {
-
+  
+  @ViewChild('r') ref:ElementRef<any>;
+  
   ngOnInit() {
+    (<any>this.ref).disabled = false;
+    this.regiForm.controls['DOB'].disable();
   }
 
   regiForm: FormGroup;  
@@ -19,32 +23,36 @@ export class ReactiveFormTwoComponent implements OnInit {
   Gender:string='';  
   Blog:string='';  
   Email:string='';  
-  IsAccepted:number=0;  
+  IsAccepted:number=0;
+
   
+
   constructor(private fb: FormBuilder) {   
   
   
     // To initialize FormGroup  
     this.regiForm = fb.group({  
-      'FirstName' : ['Sraban', Validators.required],  
+      'FirstName' : [{value:'Sraban', disabled : true}, Validators.required],  
       'LastName' : ['Kumar', Validators.required],  
       'Address' : ['aa aaa aaa aaa aaa aaa aaa aaa', Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],  
       'DOB' : ['2019-07-18', Validators.required],  
       'Gender':['Male', Validators.required],  
       'Blog':['1', Validators.required],  
       'Email':['skp.pvt@gmail.com', Validators.compose([Validators.required,Validators.email])],  
-      'IsAccepted':[true]  
-    });  
-  
+      'IsAccepted':[false]  
+    });
+
   }  
   
   // On Change event of Toggle Button  
   onChange(event:any)  
   {  
+    
     if (event.checked == true) {  
-      this.IsAccepted = 1;  
+      this.IsAccepted = 1;
+      (<any>this.ref).disabled = true;
     } else {  
-      this.IsAccepted = 0;  
+      this.IsAccepted = 0;
     }  
   }  
   
@@ -52,5 +60,7 @@ export class ReactiveFormTwoComponent implements OnInit {
   onFormSubmit(form:NgForm)  
   {  
     console.log(form, this.regiForm.value);  
-  }  
+  }
+  
+
 }
